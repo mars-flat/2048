04cccf5627e44590f827909e991b9f07e09aebc4
Shane Chen <75336695+mars-flat@users.noreply.github.com>
2021-04-08 13:58:30 -0400

    Delete twozerofoureight directory

--- twozerofoureight/frontend/RenderEvent.java
+++
@@ -1,9 +0,0 @@
-package twozerofoureight.frontend;
-
-import java.util.*;
-
-public interface RenderEvent {
-	//basically creating a local draw() function
-	public void Run();
-	public void Run(Map<String, Integer> positions);
-}
